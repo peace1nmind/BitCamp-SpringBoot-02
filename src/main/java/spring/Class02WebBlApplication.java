aba0ee05c16378277cb28a03cdc26ca3ae6f012a
user <user@HS-NOTEBOOK>
2024-10-01 13:10:53 +0900

    10.01.화 수업내용 (springboot mybatis 적용) pom.xml, application.properties 확인

---
+++ src/main/java/spring/Class02WebBlApplication.java
@@ -0,0 +1,13 @@
+package spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Class02WebBlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Class02WebBlApplication.class, args);
+	}
+
+}
